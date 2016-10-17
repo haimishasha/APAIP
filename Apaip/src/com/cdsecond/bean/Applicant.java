832af55509736d2642d6902d95aa21ede36a8ab4
Haimishasha <1417358169@qq.com>
2016-10-17 12:42:04 +0800

    init program

---
+++ Apaip/src/com/cdsecond/bean/Applicant.java
@@ -0,0 +1,14 @@
+package com.cdsecond.bean;
+
+public class Applicant {
+
+	public Applicant() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
