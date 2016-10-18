b83c298f797356d6e1e878e1647e483477687cd8
HAO1175513356 <1175513356@qq.com>
2016-10-18 16:54:30 +0800

    工具包的编写 类Tools负责将date数据类型转化为String类型 类Catalog 对话框（？？） 类DBUtil负责连接mysql数据库的包装

---
+++ Apaip/src/com/cdsecond/tools/Tools.java
@@ -0,0 +1,147 @@
+package com.cdsecond.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ * 公共方法
+ * @author zs
+ *
+ */
+public class Tools {
+	
+	/**
+	 * 判断字符串是否非空
+	 * @param str
+	 * @return boolean
+	 */
+	public static boolean isEmpty(String str){
+		if(str==null||str.equals("")){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 判断Long是否为空
+	 * @param str
+	 * @return boolean
+	 */
+	public static boolean isEmpty(Long str){
+		if(str==null||str.equals("")){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 把null转化为""
+	 * @param str
+	 * @return String
+	 */
+	public static String ToEmpty(String str){
+		if(str==null){
+			str="";
+		}
+		return str;
+
+	}
+	
+	
+	/**
+	 * 模糊查询字符串转换
+	 * @param str
+	 * @return String
+	 */
+	public static String getSelect(String str){
+		return "%"+str+"%";
+	}
+	
+	
+	/**
+	 * 字符串转化为日期格式
+	 * @param str
+	 * @return String
+	 */
+	public static String str2date(String str){
+		return "to_date('"+str+"','yyyy-mm-dd')";
+	}
+	
+	
+	/**
+	 * 日期转化为字符串格式
+	 * @param str
+	 * @return String
+	 */
+	public static String date2str(String str){
+		return "to_char('"+str+"','yyyy-mm-dd')";
+	}
+
+	
+	/**
+	 * 根据日期是否为空来截取日期，是它只显示年月日
+	 * @param date
+	 * @return String
+	 */
+	public static String getDate(String date){
+		String str=null;
+		if(isEmpty(date)){
+			str = date;
+		}else{
+			str = date.substring(0,10);
+		}
+		return str;
+	}
+	
+	/**
+	 * list倒序
+	 * @param list
+	 * @return List
+	 */
+	public static List<Catalog> getList(List<Catalog> list){
+		List<Catalog> getList = new ArrayList<Catalog>();
+		for(int i=list.size()-1;i>=0;i--){
+			getList.add(list.get(i));
+		}
+		return getList;
+	}
+	
+	/**
+	 * 截取字符串
+	 * @param str
+	 * @return String
+	 */
+	
+	public static String getStringOne2Ten(String str){
+		String get = "";
+		if(str.length()>8){
+			get=str.substring(0, 7)+"...";
+		}else{
+			get=str;
+		}
+		return get;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
