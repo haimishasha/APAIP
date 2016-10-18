014a89de7040bc9522c0a0963fbcf388c2a04a12
HAO1175513356 <1175513356@qq.com>
2016-10-18 16:50:56 +0800

    扶贫情况表，扶贫结果表dao层编写 对应功能有录入，查询，修改

--- Apaip/src/com/cdsecond/bean/AlleviationResult.java
+++ Apaip/src/com/cdsecond/bean/AlleviationResult.java
@@ -1,14 +1,31 @@
 package com.cdsecond.bean;
 
 public class AlleviationResult {
-	//private String alleviationResultID;		//帮扶结果ID
+	private int alleviationResultID;		//帮扶结果ID
+	private int applicantID;				//申请人ID
 	private String alleviationStartTime;	//帮扶开始时间
+	public AlleviationResult() {
+		super();
+	}
+	public AlleviationResult(int alleviationResultID, int applicantID,
+			String alleviationStartTime, String alleviationEndTime,
+			String registerTime, String resultDescription,
+			String alleviationStatus, String registrant) {
+		super();
+		this.alleviationResultID = alleviationResultID;
+		this.applicantID = applicantID;
+		this.alleviationStartTime = alleviationStartTime;
+		this.alleviationEndTime = alleviationEndTime;
+		this.registerTime = registerTime;
+		this.resultDescription = resultDescription;
+		this.alleviationStatus = alleviationStatus;
+		this.registrant = registrant;
+	}
 	private String alleviationEndTime;		//帮扶结束时间
 	private String registerTime;			//登记时间
 	private String resultDescription;		//帮扶结果描述
 	private String alleviationStatus;		//帮扶状态
 	private String registrant;				//登记人
-
 	public String getAlleviationStartTime() {
 		return alleviationStartTime;
 	}
@@ -20,6 +37,31 @@ public class AlleviationResult {
 	}
 	public void setAlleviationEndTime(String alleviationEndTime) {
 		this.alleviationEndTime = alleviationEndTime;
+	}
+	public void setRegistrant(String registrant) {
+		this.registrant = registrant;
+	}
+	public int getAlleviationResultID() {
+		return alleviationResultID;
+	}
+	public void setAlleviationResultID(int alleviationResultID) {
+		this.alleviationResultID = alleviationResultID;
+	}
+	public int getApplicantID() {
+		return applicantID;
+	}
+	public void setApplicantID(int applicantID) {
+		this.applicantID = applicantID;
+	}
+	@Override
+	public String toString() {
+		return "AlleviationResult [alleviationResultID=" + alleviationResultID
+				+ ", applicantID=" + applicantID + ", alleviationStartTime="
+				+ alleviationStartTime + ", alleviationEndTime="
+				+ alleviationEndTime + ", registerTime=" + registerTime
+				+ ", resultDescription=" + resultDescription
+				+ ", alleviationStatus=" + alleviationStatus + ", registrant="
+				+ registrant + ", toString()=" + super.toString() + "]";
 	}
 	public String getRegisterTime() {
 		return registerTime;
@@ -42,32 +84,6 @@ public class AlleviationResult {
 	public String getRegistrant() {
 		return registrant;
 	}
-	@Override
-	public String toString() {
-		return "AlleviationResult [alleviationStartTime="
-				+ alleviationStartTime + ", alleviationEndTime="
-				+ alleviationEndTime + ", registerTime=" + registerTime
-				+ ", resultDescription=" + resultDescription
-				+ ", alleviationStatus=" + alleviationStatus + ", registrant="
-				+ registrant + ", toString()=" + super.toString() + "]";
-	}
-	public void setRegistrant(String registrant) {
-		this.registrant = registrant;
-	}
-	public AlleviationResult() {
-		super();
-	}
-	public AlleviationResult(String alleviationStartTime,
-			String alleviationEndTime, String registerTime,
-			String resultDescription, String alleviationStatus,
-			String registrant) {
-		super();
-		this.alleviationStartTime = alleviationStartTime;
-		this.alleviationEndTime = alleviationEndTime;
-		this.registerTime = registerTime;
-		this.resultDescription = resultDescription;
-		this.alleviationStatus = alleviationStatus;
-		this.registrant = registrant;
-	}
+
 
 }
