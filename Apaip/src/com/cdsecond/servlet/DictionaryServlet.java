b88efd13ca19d8debbbcc6c76604e0314a173ab3
haohan <haohan@haohan-PC>
2016-10-20 11:21:52 +0800

    zzz

--- Apaip/src/com/cdsecond/servlet/DictionaryServlet.java
+++ Apaip/src/com/cdsecond/servlet/DictionaryServlet.java
@@ -2,22 +2,18 @@ package com.cdsecond.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.cdsecond.bean.Dictionary;
-import com.cdsecond.dao.DictionaryDao;
-
-public class DictionarySevlet extends HttpServlet {
+public class DictionaryServlet extends HttpServlet {
 
 	/**
 	 * Constructor of the object.
 	 */
-	public PovertyMeasureSevlet() {
+	public DictionaryServlet() {
 		super();
 	}
 
@@ -42,7 +38,7 @@ public class DictionarySevlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		doPost(request, response);
+		doPost(request,response);
 	}
 
 	/**
@@ -58,23 +54,20 @@ public class DictionarySevlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		request.setCharacterEncoding("utf-8");
-		response.setCharacterEncoding("utf-8");
-		String main=request.getParameter("main");
-
-		if(main.equals("EditStart")){
-			EditStart(request, response);			
-		}else if(main.equals("edit")){
-			edit(request,response);
-		}else if(main.equals("List")){
-			list(request,response);
-		}else if(main.equals("Det")){
-			det(request, response);
-		}else if(main.equals("delete")){
-			delete(request, response);
-		}
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the POST method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
 	}
-	
 
 	/**
 	 * Initialization of the servlet. <br>
@@ -84,54 +77,5 @@ public class DictionarySevlet extends HttpServlet {
 	public void init() throws ServletException {
 		// Put your code here
 	}
-	/**
-	 * 初始进入添加界面
-	 * @param request
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	protected void EditStart(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
-		/**
-		 * 修改时获取到需要修改的人物信息，若无信息，则为添加界面
-		 */
-		String ids = request.getParameter("id");
-		request.setAttribute("id", ids);
-		int id = Integer.parseInt(ids);
-		Dictionary dictionary;
-		try {
-			dictionary = DictionaryDao.getOneDictionary(id);
-			request.setAttribute("dictionary", dictionary);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		request.getRequestDispatcher("dictionary/dictionaryEdit.jsp").forward(request, response);
-		
-	}
-	/**
-	 * 添加界面提交
-	 * @param request
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	protected void edit(HttpServletRequest request,HttpServletResponse response) {
-		
-		String ids = request.getParameter("id");
-		Dictionary a = new Dictionary();
-		a.setTitle(request.getParameter("act_title"));
-		a.setContext(request.getParameter("act_content"));
-		a.setState(request.getParameter("act_state"));
-		
-		
-		
-		
-		
-	}
-	
-	
-	
-	
 
 }
